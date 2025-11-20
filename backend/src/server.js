65cf0118f9facc55157e19b5e869e18d1c4f4e88
cowsaymoo <youremail@example.com>
2025-11-20 08:51:55 +0700

    Update code / Build fix / ...

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,9 +4,12 @@ import tasksRoutes from './routes/tasksRoutes.js';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path'
 
 dotenv.config();
 const PORT = process.env.PORT || 5001;
+
+const __dirname = path.resolve();
 
 
 const app = express();
@@ -17,6 +20,10 @@ app.use(cors({origin: "http://localhost:5173"}));
 app.use(express.json());
 
 app.use("/api/tasks", tasksRoutes);
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+
 
 connectDB().then(() => {
     app.listen(PORT, () => {
